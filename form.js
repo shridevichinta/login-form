bb61e1ccef731261b7b3089fbd7d98942490e2df
shridevichinta <94972316+shridevichinta@users.noreply.github.com>
2021-11-24 18:47:23 +0530

    Add files via upload
    updated file from annapurna local pc

---
+++ form.js
@@ -0,0 +1,10 @@
+var first_name = document.getElementById("first_name")
+var last_name = document.getElementById("last_name")
+
+
+function form_submit(){
+    console.log("First Name: " + first_name.value)
+    console.log("Last Name: " + last_name.value)
+
+
+}
